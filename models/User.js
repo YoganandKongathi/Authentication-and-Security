9303aa35ebb25c3b927d82e66236764743d47bf3
YoganandKongathi <135534288+YoganandKongathi@users.noreply.github.com>
2023-07-04 18:22:51 +0530

    Level 1 - Email and Password

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption')
+const {Schema, model} = mongoose
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+const secret = "Thisisourlittlesecret."
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password'] })
+
+const User = model('User', userSchema)
+
+module.exports = User
