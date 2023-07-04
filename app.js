1b63473d99a05c7ee95335db356a984df7ac89b1
YoganandKongathi <135534288+YoganandKongathi@users.noreply.github.com>
2023-07-04 22:57:39 +0530

    Level 4 - Salting and Hashing with bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const User = require('./models/User.js')
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 
 const app = express()
 
@@ -27,34 +28,41 @@ app.get('/register', function(req,res){
 
 app.post('/login', function(req,res){
     const username = req.body.username
-    const password = md5(req.body.password)
+    const password = req.body.password
 
     User.findOne({email: username}, function(err, foundUser){
         if (err) {
             console.log(err);
         } else {
             if (foundUser){
-                if (foundUser.password === password){
-                    res.render('secrets')
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true){
+                        res.render('secrets')
+                    }
+                });
             }
         }
     })
 })
 
 app.post('/register', function(req,res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, function(err,hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+    
+        newUser.save(function(err){
+            if(!err){
+                res.render('secrets')
+            } else {
+                console.log(err);
+            }
+        })
     })
 
-newUser.save(function(err){
-    if(!err){
-        res.render('secrets')
-    } else {
-        console.log(err);
-    }
-})
+    
 })
 
 app.listen(3000, ()=>{
